12da1c4601ebd4a13697611c4385db22427bdabf
Holger <holgus103@gmail.com>
2015-11-15 23:07:43 +0100

    Missing test

--- test/pingMyNetwork/test/IPV4AddressTest.java
+++ test/pingMyNetwork/test/IPV4AddressTest.java
@@ -8,28 +8,77 @@ import org.junit.*;
  *
  * @author Jakub
  */
-public class IPV4AddressTest {
+public class IPv4AddressTest {
+    private static final String LOOPBACK = "127.0.0.1";
+    private static final String RESCRICTED_IP = "0.0.0.0";
+    private static final String MAX_IP = "255.255.255.255";
+    private static final String INVALID_IP = "512.512.512.512";
+    private static final String GOOGLE_DNS = "8.8.8.8";
     IPv4Address address;
     
+    /**
+     * Tests the toString method of IPV4Address
+     */
     @Test
     public void testToString(){
         try{
-        
-            this.address = new IPv4Address("127.0.0.1",IPv4Address.DEFAULT_MASK);
-            assertEquals("127.0.0.1",this.address.toString());
+            this.address = new IPv4Address(this.LOOPBACK);
+            assertEquals(this.LOOPBACK,this.address.toString());
         }
         catch(InvalidIPAddressException | IndexOutOfBoundsException | NumberFormatException e){
-                       
+            fail("Test failed");           
+        }
+        
+        try{
+            this.address = new IPv4Address(RESCRICTED_IP);
+            assertEquals(RESCRICTED_IP,this.address.toString());
+        }
+        catch(InvalidIPAddressException | IndexOutOfBoundsException | NumberFormatException e){
+            fail("Test failed");           
+        }
+        
+        try{
+            this.address = new IPv4Address(this.MAX_IP);
+            assertEquals(MAX_IP,this.address.toString());
+        }
+        catch(InvalidIPAddressException | IndexOutOfBoundsException | NumberFormatException e){
+            fail("Test failed");           
+        }
+        
+        try{
+            this.address = new IPv4Address(this.INVALID_IP);
+            fail("Test failed");
+        }
+        catch(InvalidIPAddressException | IndexOutOfBoundsException | NumberFormatException e){         
         }
     }
+
+    /**
+     *
+     */
     @Test
-    public void testISReachable(){
+    public void testIsReachable(){
         try{
-            this.address = new IPv4Address("127.0.0.1",IPv4Address.DEFAULT_MASK);
+            this.address = new IPv4Address(this.LOOPBACK);
             assertTrue(this.address.isReachable(1000, false));
         }
         catch(IndexOutOfBoundsException |InvalidIPAddressException |NumberFormatException |IOException
                 |IllegalAccessError e){
         }
-    }
+        
+        try{
+            this.address = new IPv4Address(GOOGLE_DNS);
+            assertTrue(this.address.isReachable(1000, false));
+        }
+        catch(IndexOutOfBoundsException |InvalidIPAddressException |NumberFormatException |IOException
+                |IllegalAccessError e){
+        }
+        try{
+            this.address = new IPv4Address(RESCRICTED_IP);
+            assertFalse(this.address.isReachable(1000, false));
+        }
+        catch(IndexOutOfBoundsException |InvalidIPAddressException |NumberFormatException |IOException
+                |IllegalAccessError e){
+        }
+    }  
 }
